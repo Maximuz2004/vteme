ebb05dc378409d5a43abb4663d2683a0aff2294a
Максим Титов <matitov2004@gmail.com>
2023-09-07 14:22:05 +0300

    templates for account

--- vteme/account/urls.py
+++ vteme/account/urls.py
@@ -9,5 +9,48 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            success_url='done',
+            template_name='registration/password_change_form.html'
+        ),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='registration/password_change_done.html'
+        ),
+        name='password_change_done'
+    ),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+    path('', views.dashboard, name='dashboard')
 ]
