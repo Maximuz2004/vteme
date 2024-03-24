844b51c5d05a7bab3a47b00c171851aa2b485d8d
Максим Титов <matitov2004@gmail.com>
2024-03-24 16:38:45 +0300

    google oauth add

--- vteme/account/authentication.py
+++ vteme/account/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 
+from .models import Profile
+
 
 class EmailAuthBackend:
 
@@ -19,3 +21,5 @@ class EmailAuthBackend:
             return None
 
 
+def create_profile(backend, user, *args, **kwargs):
+    Profile.objects.get_or_create(user=user)
--- vteme/vteme/settings.py
+++ vteme/vteme/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,23 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 ]
 
 # Internationalization
